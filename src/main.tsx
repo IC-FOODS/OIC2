06f4c2de29cd75a555446ed1180880ae7b7074ed
mateolan <mateolan@gmail.com>
2025-04-05 08:42:35 +0200

    Fix: use AppShell as root layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import AppShell from './components/AppShell';
+import { AppShell } from './layout/AppShell';
 
-/**
- * Main application entry point.
- */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppShell />
